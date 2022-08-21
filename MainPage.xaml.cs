cc7f8259a7c703b83761ae846c8f658e5906137b
SpookySnek <carleric.olsson@gmail.com>
2022-08-21 23:03:45 +0200

    Implemented dirty "From" & "To" values for number generation
    - This will be fixed to handle exceptions and bad inputs
    - Design will be cleaned up

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,10 +1,42 @@
 ﻿namespace SampleApp;
 
 public partial class MainPage : ContentPage
-{
-	Random number = new Random();
 
-	public MainPage()
+
+{
+
+
+    void OnTextChanged(object sender, EventArgs e)
+    {
+        var text = ((Entry)sender).Text;
+        Console.WriteLine(text);
+    }
+
+    Random number = new Random();
+
+	private int To()
+    {
+        int parsedTo;
+        bool success = int.TryParse(ToNumber.Text, out parsedTo);
+        if (success)
+        {
+            return parsedTo;
+        }
+        return 0;
+    }
+
+    private int From()
+    {
+        int parsedFrom;
+        bool success = int.TryParse(FromNumber.Text, out parsedFrom);
+        if (success)
+        {
+            return parsedFrom;
+        }
+        return 0;
+    }
+
+    public MainPage()
 	{
 		InitializeComponent();
 	}
@@ -12,7 +44,7 @@ public partial class MainPage : ContentPage
 	private void OnCounterClicked(object sender, EventArgs e)
 	{
 
-			NumberDisplay.Text = $"{number.Next()}";
+			NumberDisplay.Text = $"{number.Next(From(), To())}";
 
 		SemanticScreenReader.Announce(NumberDisplay.Text);
 	}
