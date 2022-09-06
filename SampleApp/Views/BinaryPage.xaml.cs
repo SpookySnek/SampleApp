857bd8a0201ef97ed08092638616df80c40004bd
SpookySnek <carleric.olsson@gmail.com>
2022-09-06 12:55:15 +0200

    Implemented working Binary Translator in the app
    - The app currently breaks if you try tro translate binary with blankspaces. Should be a very easy fix

--- SampleApp/Views/BinaryPage.xaml.cs
+++ SampleApp/Views/BinaryPage.xaml.cs
@@ -6,4 +6,22 @@ public partial class BinaryPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private string translatedInput;
+
+    private void OnTranslate(object sender, EventArgs e)
+    {
+        string enteredInput = BinaryText.Text;
+
+        translatedInput = SampleApp.Core.BinaryTranslator.TranslateToString(enteredInput);
+
+        if (!string.IsNullOrEmpty(translatedInput))
+        {
+            TranslateButton.Text = translatedInput;
+        }
+        else
+        {
+            TranslateButton.Text = "Invalid input!";
+        }
+    }
 }
