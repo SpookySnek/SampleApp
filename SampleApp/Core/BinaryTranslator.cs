00cb1154dbdf4021ed2481e4596a4d19adeefd50
SpookySnek <carleric.olsson@gmail.com>
2022-09-06 12:19:41 +0200

    Implemented BinaryTranslator.cs in Core folder

---
+++ SampleApp/Core/BinaryTranslator.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace SampleApp.Core;
+public static class BinaryTranslator
+{
+    public static byte[] GetBytesFromBinaryString(String binary)
+    {
+        var list = new List<byte>();
+
+        for (int i = 0; i < binary.Length; i += 8)
+        {
+            string t = binary.Substring(i, 8);
+
+            list.Add(Convert.ToByte(t, 2));
+        }
+
+        return list.ToArray();
+    }
+    public static string TranslateToString(string raw)
+    {
+        return Encoding.ASCII.GetString(GetBytesFromBinaryString(raw));
+    }
+}
+
